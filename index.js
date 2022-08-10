5bd6d5dbe75af2c8ca17c0fc924bd2edfd1b9395
Ram-Santosh <ram.santosh@techmojo.in>
2022-08-10 15:33:35 +0530

    basic game working

--- index.js
+++ index.js
@@ -9,11 +9,48 @@ var mouse = {
     y: canvas.height/2
 }
 
+var border = canvas.getBoundingClientRect();
+
 document.addEventListener("mousedown",(e)=>{
-    var border = canvas.getBoundingClientRect();
     mouse.x = e.x - border.left;
     mouse.y = e.y - border.top;
 })
+
+class NPC {
+    constructor(){
+        this.x = -25 - Math.random()*canvas.width
+        this.y = Math.random()*canvas.height
+        this.size = Math.random()*50 + 10
+        this.speed = Math.random()*5
+    }
+
+    update(){
+        if (this.x > canvas.width + this.size){
+            this.reset();
+        }
+        else this.x += this.speed;
+
+    }
+
+    reset(){
+        this.x = -25 - Math.random()*canvas.width
+        this.y = Math.random()*canvas.height
+        this.size = Math.random()*50 + 10
+        this.speed = Math.random()*5
+    }
+
+    draw(){
+        ctx.fillStyle = "green"
+        ctx.beginPath()
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.fill()
+    }
+}
+
+var listOfNpc = [];
+for (let index = 0; index < 10; index++) {
+    listOfNpc.push(new NPC ())    
+}
 
 class MC {
     constructor(){
@@ -27,11 +64,11 @@ class MC {
         var dy = this.y - mouse.y;
 
         if (mouse.x != this.x){
-            this.x -= dx/30;
+            this.x -= dx/20;
         }
 
         if (mouse.y != this.y){
-            this.y -= dy/30;
+            this.y -= dy/20;
         }
     }
 
@@ -44,11 +81,46 @@ class MC {
 }
 
 const mc = new MC ();
+var score = 0;
+var gameOver = false;
 
 function animate() {
     ctx.clearRect(0, 0, 800, 600);
+    
+    ctx.font = "30px Arial";
+    ctx.strokeText("Score:" + score, 10, 30);
+
+    for (let index = 0; index < listOfNpc.length; index++) {
+        listOfNpc[index].update();
+        listOfNpc[index].draw();
+    }
+
     mc.update();
     mc.draw();
+
+    for (let index = 0; index < listOfNpc.length; index++) {
+        var isChecked = false;
+        var dx = Math.abs(mc.x - listOfNpc[index].x);
+        var dy = Math.abs(mc.y - listOfNpc[index].y);
+        var distance = Math.sqrt(dx*dx + dy*dy);
+        if (distance <= mc.size/2 + listOfNpc[index].size/2){
+            if (listOfNpc[index].size <= mc.size){
+                if (!isChecked){
+                    if (!gameOver) score++;
+                }
+                isChecked = true;
+
+                listOfNpc[index].reset();
+            }
+            else {
+                gameOver = true;
+            }
+        }
+    }
+    if (gameOver){
+        ctx.font = "100px Arial";
+        ctx.strokeText("Game Over", canvas.width/2, canvas.height/2);
+    }
     requestAnimationFrame(animate);
 }
 
